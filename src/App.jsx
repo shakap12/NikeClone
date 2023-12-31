c16f3aa72de71d5dd70fe9c8c90452e1ad3bce53
shakap12 <shashwatkapoor14@gmail.com>
2024-01-01 01:51:50 +0530

    Completed UI

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { CustomerReviews, PopularProducts, Footer, Hero, Services, Subscribe, SuperQuality } from "./sections";
+
+import Nav from "./Components/Nav";
+import Offers from "./sections/Offers";
+import { Suspense } from "react";
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero></Hero>
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <Offers />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+
+  </main>
+)
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality"
+import SpecialOffers from "./SuperQuality"
+import CustomerReviews from "./CustomerReviews"
+import Footer from "./Footer"
+import Services from "./Services"
+import Subscribe from "./Subscribe"
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    SpecialOffers,
+    CustomerReviews,
+    Footer,
+    Services,
+    Subscribe
+}
